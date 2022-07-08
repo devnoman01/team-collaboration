f6659f72e22694c2de69801e999492abbf4e3e3e
devnoman01 <devnoman01@gmail.com>
2022-07-08 20:43:36 +0600

    info updated by noman

--- src/App.js
+++ src/App.js
@@ -4,7 +4,26 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div className="container"></div>
+      <div className="container mx-auto p-5">
+        <div className="grid grid-cols-3 gap-4">
+          <div class="card w-96 bg-green-50 border shadow-xl">
+            <figure class="px-10 pt-10">
+              <img
+                src="https://avatars.githubusercontent.com/u/67538033?v=4"
+                alt="Shoes"
+                class="rounded-xl"
+              />
+            </figure>
+            <div class="card-body items-center text-center">
+              <h2 class="card-title">Md.Abdullah Al Noman</h2>
+              {/* <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div class="card-actions">
+                <button class="btn btn-primary">Buy Now</button>
+              </div> */}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
