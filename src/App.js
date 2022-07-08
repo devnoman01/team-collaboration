b0e8ea8daf9b279b0ec059e95471afd782271c76
Minhazul Alam <78158511+akib-chy@users.noreply.github.com>
2022-07-08 21:25:17 +0600

    card/akib brance added done

--- src/App.js
+++ src/App.js
@@ -64,6 +64,24 @@ function App() {
           </div>
 
           {/*  */}
+          <div class="card w-full bg-green-50 border shadow-xl">
+            <figure class="px-10 pt-10">
+              <img
+                src="https://avatars.githubusercontent.com/u/78158511?v=4"
+                alt="Shoes"
+                class="rounded-xl"
+              />
+            </figure>
+            <div class="card-body items-center text-center">
+              <h2 class="card-title">MInhajul Alam</h2>
+              {/* <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div class="card-actions">
+                <button class="btn btn-primary">Buy Now</button>
+              </div> */}
+            </div>
+          </div>
+
+          {/*  */}
         </div>
       </div>
     </div>
