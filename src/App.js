578fe1a3733716719b27b90a680a843b73d5c695
zannatreya <zannatulbintabahar@yahoo.com>
2022-07-08 21:14:51 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -27,6 +27,29 @@ function App() {
           {/*  */}
         </div>
       </div>
+      <div className="container mx-auto p-5">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+          {/* user: noman */}
+          <div class="card w-full bg-green-50 border shadow-xl">
+            <figure class="px-10 pt-10">
+              <img
+                src="https://avatars.githubusercontent.com/u/67538033?v=4"
+                alt="Shoes"
+                class="rounded-xl"
+              />
+            </figure>
+            <div class="card-body items-center text-center">
+              <h2 class="card-title">Zannatul Binta Bahar</h2>
+              {/* <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div class="card-actions">
+                <button class="btn btn-primary">Buy Now</button>
+              </div> */}
+            </div>
+          </div>
+
+          {/*  */}
+        </div>
+      </div>
     </div>
   );
 }
