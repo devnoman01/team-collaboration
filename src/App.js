7b6af1c31724822b234a496a17ce820b538b9847
Minhazul Alam <78158511+akib-chy@users.noreply.github.com>
2022-07-21 21:49:48 +0600

    complete userflow and navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 // import logo from "./logo.svg";
 import "./App.css";
 // import Footer from "./Footer/Footer";
-// import Navbar from "./Navbar/Navbar";
-import Account from "./Account/Account";
+import Navbar from "./Navbar/Navbar";
+// import Account from "./Account/Account";
+import HeroSection from "./HeroSection/HeroSection";
+import UserFlow from "./UserFlow/UserFlow";
 
 function App() {
   return (
-    <div className="my-32">
-      {/* <Footer /> */}
-      {/* <Navbar /> DONE */}
-      <Account />
+    <div className="">
+      <Navbar />
+      <UserFlow />
+      <HeroSection />
     </div>
   );
 }
--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -3,6 +3,7 @@ import "./Navbar.css";
 import { MenuIcon, XIcon } from "@heroicons/react/solid";
 
 const Navbar = () => {
+  // ALL ROUTE
   const link = [
     { id: 1, name: "Home", link: "/home" },
     { id: 2, name: "About", link: "/about" },
@@ -10,18 +11,20 @@ const Navbar = () => {
     { id: 4, name: "Contact", link: "/contact" },
     { id: 5, name: "Other", link: "/other" },
   ];
+  // RESPONSIVE TOGGLER BTN STATE
   const [open, setOpen] = useState(false);
   return (
     <div className="container mx-auto lg:m-7 ">
       <div className="bg-base-200 p-1 lg:px-8 lg:rounded-2xl md:px-4">
         <nav className="flex items-center justify-between">
-          {/* <div className="flex items-center justify-between "> */}
+          {/* PROJECT LOGO */}
           <div>
             <a href="#">Sohoj Pay</a>
           </div>
 
           <div className="ml-20">
             {" "}
+            {/* NAV ITEM */}
             <ul
               className={`lg:flex w-80 h-72 lg:h-auto lg:w-full block lg:items-center navbar absolute duration-500 ease-in lg:static bg-indigo-200 lg:bg-base-200 top-12  ${
                 open ? "left-[-10px] top-12" : "left-[-380px]"
@@ -32,6 +35,7 @@ const Navbar = () => {
                   <a href={item.link}>{item.name}</a>
                 </li>
               ))}
+              {/* RESPONSIVE LOGIN OR SIGN UP  BUTTON */}
               <div className="flex items-center justify-center md:hidden">
                 <button className="btn bg-sky-600 hover:bg-sky-700 border-0 btn-sm mr-3">
                   Login
@@ -40,7 +44,8 @@ const Navbar = () => {
                   Sign Up
                 </button>
               </div>
-              <button className="btn bg-transparent border-2 text-black hover:bg-black hover:text-white border-0 btn-sm lg:mx-4 hidden lg:block">
+              {/* LARGE DEVICE LOGIN OR SIGN UP BUTTON */}
+              <button className="btn bg-transparent border-2 text-black hover:bg-black hover:text-white btn-sm lg:mx-4 hidden lg:block">
                 Login
               </button>
               <button className="btn bg-red-600 hover:bg-red-700 border-0 btn-sm hidden lg:block">
@@ -48,9 +53,7 @@ const Navbar = () => {
               </button>
             </ul>
           </div>
-          {/* </div> */}
-          {/* <div className="flex items-center hidden md:block"> */}
-          {/* </div> */}
+          {/* NAVBAR TOGGLER ICON */}
           <div onClick={() => setOpen(!open)} className="w-10 h-10 lg:hidden">
             {open ? <XIcon></XIcon> : <MenuIcon></MenuIcon>}
           </div>
