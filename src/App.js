ee68d46bef0c31dd5edd05917685d6dc16487905
shahadat2772 <shahadat2772@gmailcom>
2022-07-22 09:25:17 +0600

    slidly modified the nav

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import Navbar from "./Pages/Shared/Navbar/Navbar";
 import CoreFeature from "./Pages/Home/Sections/CoreFeatures/CoreFeatures";
 import CoreFeatures from "./Pages/Home/Sections/CoreFeatures/CoreFeatures";
 import WhyUs from "./Pages/Home/Sections/WhyUs/WhyUs";
+import Banner from "./Pages/Home/Sections/Banner/Banner/Banner";
+import HeroSection from "./Pages/Home/Sections/Banner/HeroSection/HeroSection";
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <Banner></Banner>
+      <HeroSection></HeroSection>
       <UserFlow></UserFlow>
       <CoreFeature></CoreFeature>
       <WhyUs />
