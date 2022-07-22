64e38cc19436218bdaf849f5376f887e1f1cc1ff
shahadat2772 <shahadat2772@gmailcom>
2022-07-22 08:07:01 +0600

    home page updated

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import UserFlow from "./Pages/Home/Sections/UserFlow/UserFlow";
 import Footer from "./Pages/Shared/Footer/Footer";
 import Navbar from "./Pages/Shared/Navbar/Navbar";
+import CoreFeature from "./Pages/Home/Sections/CoreFeatures/CoreFeatures";
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
       <UserFlow></UserFlow>
+      <CoreFeature></CoreFeature>
       <Footer></Footer>
     </div>
   );
