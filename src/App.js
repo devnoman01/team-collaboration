3f92544997083449d6d0031aeb5d5833ef1a4f9f
mahfuzswaron <mahfuzswaron@gmail.com>
2022-07-22 09:49:34 +0600

    theme color set and font family added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import UserFlow from "./Pages/Home/Sections/UserFlow/UserFlow";
 import Footer from "./Pages/Shared/Footer/Footer";
 import Navbar from "./Pages/Shared/Navbar/Navbar";
 import CoreFeature from "./Pages/Home/Sections/CoreFeatures/CoreFeatures";
-import CoreFeatures from "./Pages/Home/Sections/CoreFeatures/CoreFeatures";
 import WhyUs from "./Pages/Home/Sections/WhyUs/WhyUs";
+import HeroSection from "./Pages/Home/Sections/Banner/HeroSection/HeroSection";
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <HeroSection />
       <UserFlow></UserFlow>
       <CoreFeature></CoreFeature>
       <WhyUs />
